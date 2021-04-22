4287701d84e687780f40f81a96f7263815cbe1b0
João Paulo Maida <jpmaida@gmail.com>
2021-04-22 15:49:26 -0300

    montagem de url

--- src/app/services/todo-api.service.ts
+++ src/app/services/todo-api.service.ts
@@ -8,13 +8,16 @@ import { catchError, map } from 'rxjs/operators';
 })
 export class TodoApiService {
 
-  private SERVER_BASE_URL = "/api/todolist";
+  //private BASE_SERVER_URL = "http://localhost:8080";
+  private BASE_SERVER_URL = "";
+  private BASE_API_URL = "/api/todolist";
+  private BASE_FULL_URL = this.BASE_SERVER_URL + this.BASE_API_URL;
 
   constructor(private httpClient: HttpClient) { }
 
   public all(){
 	  return this.httpClient
-      .get(this.SERVER_BASE_URL)
+      .get(this.BASE_FULL_URL)
       .pipe(
         catchError(error => { return this.handleError(error); })
       );;
@@ -22,7 +25,7 @@ export class TodoApiService {
 
   public new(body: any){
     return this.httpClient
-      .post(this.SERVER_BASE_URL,body)
+      .post(this.BASE_FULL_URL,body)
       .pipe(
         catchError(error => { return this.handleError(error); })
       );
@@ -30,7 +33,7 @@ export class TodoApiService {
 
   public delete(id: number){
     return this.httpClient
-      .delete(this.SERVER_BASE_URL+"/"+id)
+      .delete(this.BASE_FULL_URL+"/"+id)
       .pipe(
         catchError(error => { return this.handleError(error); })
       );
@@ -38,7 +41,7 @@ export class TodoApiService {
 
   public replace(toDo: any): any{
     return this.httpClient
-      .put(this.SERVER_BASE_URL+"/"+toDo.id, toDo)
+      .put(this.BASE_FULL_URL+"/"+toDo.id, toDo)
       .pipe(
         catchError(error => {
           if(error.status == 400){
